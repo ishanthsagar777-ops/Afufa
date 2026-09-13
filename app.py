2f111e4e452150409a30ad5f006a2c839258f2e8
ishanthsagar777-ops <ishanthsagar777@gmail.com>
2026-09-13 23:52:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
 st.title("Welcome to Afufa App!")
-st.write("Your Streamlit app is up and running successfully
-st.title("Welcome to Afufa App!")
 st.write("Your Streamlit app is up and running successfully.")
